450161c7665746d65255d823b3f886a1b0392498
AmitKus <amit.kushwah@gmail.com>
2020-10-13 22:28:26 -0500

    update fetch

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
-unzip packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
+unzip -d packages/regression_model/regression_model/datasets/ packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip 
 ls -lh packages/regression_model/regression_model/datasets/*
